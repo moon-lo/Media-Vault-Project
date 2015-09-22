0df551d72bd10ffdb147dcdaf43a1bacdc0bbb30
Tomo-Shortty <tomo_007@live.com.au>
2015-09-22 17:59:49 +1000

    Experimental change to directory.php
    I'm not sure if this will work. My previous code didn't work because it was looking for posted data from index.php. However, index.php does not post to this page. Since the username is the current session I will try setting accountName to that.

--- Media-Vault-Project/directory.php
+++ Media-Vault-Project/directory.php
@@ -9,10 +9,7 @@
     include ROOT_DIR . '/php-files/directory_header.php';
     
     //Determine the username of the account
-    if (isset($_POST['Login']))
-    {
-    	$accountName = $_POST['username'];
-    }
+    $accountName = $_SESSION['isUser'];
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
