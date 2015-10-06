73659fe375ac4fc594ad29d2c0095d2c39a74c59
Tomo-Shortty <tomo_007@live.com.au>
2015-10-06 16:36:22 +1000

    Added new page.
    This page should fix the session issues regarding logging out. If the
    user signs out and then clicks the back button in their browser, they
    should see this page instead of their directory.

---
+++ Media-Vault-Project/logout.php
@@ -0,0 +1,17 @@
+<?php
+	$pdo = new PDO('mysql:host=localhost;dbname=mediavault', 'root', 'password');
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+	session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Sorry</title>
+</head>
+<body>
+You must be signed in to access this page. To sign in with your account click <a href="index.php">here</a>. If you do not have an account you can sign up for one by clicking <a href="signup.php">here</a>.
+</body>
+</html>
