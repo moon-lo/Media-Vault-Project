f4416a396f7d56b5445cd908cec0dc99c41e894f
BenMcC <n8614521@LAB-GPS500-12.qut.edu.au>
2015-10-14 10:38:17 +1000

    white screen fix

--- Media-Vault-Project/share.php
+++ Media-Vault-Project/share.php
@@ -12,19 +12,17 @@
 		
 		$results = queryDB($query);
 		
-		if(mysql_num_rows($results) != 1){
-			echo "file missing, or link expired.";
-		}else{
-			$filename = $results[0]['filename'];
-			$location = $results[0]['location'];
-			downloadFile($filename, $location);
-			
-			$remove = 'DELETE FROM downloads WHERE fileId = :fileId';
-			$parameters = array(
-				':fileId' => $fileId
-			);
-			alterDB($remove, $parameters);
-		}
+
+		$filename = $results[0]['filename'];
+		$location = $results[0]['location'];
+		downloadFile($filename, $location);
+		echo "downloading file";
+		$remove = 'DELETE FROM downloads WHERE fileId = :fileId';
+		$parameters = array(
+			':fileId' => $fileId
+		);
+		alterDB($remove, $parameters);
+		
 		$pdo = null;
 
 		
